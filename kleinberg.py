e9af9dca99fe33a94e2c9242056a38e3e3698eae
Minh-Tue Vo <minhtuev@mit.edu>
2012-12-02 14:58:10 -0500

    [changing kleinberg - adding helper method]
    [changing kleinberg - adding helper method]

--- kleinberg.py
+++ kleinberg.py
@@ -33,40 +33,42 @@ def binomial_sample(N, r, count = 0):
 # input: N, k, W
 # output: (k-size list of candidates, sample population)
 def kleinberg(N, k, W):
-    print (N, k, W)
-    if k == 1:
-        # temporary array for the data
-        temp = W[:]
-        # sampling population
-        samples = W[:N]
-        # removing the sample from the queue
-        for i in range(N):
-            W.pop(0)
-        # return chosen value + sample population
-        return ([select_simple(N, temp)], samples)
-    else:
-        temp = W[:]
-        # getting the sample size from binomial distribution
-        m = binomial_sample(N, 0.5)
+    def kleinberg_helper(N,k,W):
+        if k == 1:
+            # temporary array for the data
+            temp = W[:]
+            # sampling population
+            samples = W[:N]
+            # removing the sample from the queue
+            for i in range(N):
+                W.pop(0)
+            # return chosen value + sample population
+            return ([select_simple(N, temp)], samples)
+        else:
+            temp = W[:]
+            # getting the sample size from binomial distribution
+            m = binomial_sample(N, 0.5)
 
-        # choosing k/2 candidate from the samples
-        (candidates, samples) = kleinberg(m, k/2, W)
-        
-        # sorting the samples, from smallest to largest
-        samples.sort()
-        # reverse the samples, now from largest to smallest
-        samples.reverse()
-        # take the threshold to be the k/2th largest item in the sample
-        threshold = samples[k/2]
-        
-        for i in range(N-m):
-            if len(candidates) < k:
-                u = W.pop(0)
-                if (u>threshold):
-                    candidates.append(u)
-        
-        return (candidates, temp[:N])
+            # choosing k/2 candidate from the samples
+            (candidates, samples) = kleinberg_helper(m, k/2, W)
+            
+            # sorting the samples, from smallest to largest
+            samples.sort()
+            # reverse the samples, now from largest to smallest
+            samples.reverse()
+            # take the threshold to be the k/2th largest item in the sample
+            threshold = samples[k/2]
+            
+            for i in range(N-m):
+                if len(candidates) < k:
+                    u = W.pop(0)
+                    if (u>threshold):
+                        candidates.append(u)
+
+            return (candidates, temp[:N])
+    candidates = kleinberg_helper(N, k, W)[0]
+    return candidates
 
     
 print select_simple(10, [4,1,3,5,9,0,2,8,6,7])
-print kleinberg(10, 3, [4,1,3,5,9,0,2,8,6,7])[0]
+print kleinberg(10, 3, [4,1,3,5,9,0,2,8,6,7])
